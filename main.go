1148d5c46baade725308f44d8597bba59730daa6
lazyturtlez <robertbracamonte803@gmail.com>
2024-12-08 15:25:23 -0800

    2: added basic structure for adding, deleting, and listing tasks from the json file where they will be held

--- main.go
+++ main.go
@@ -1,55 +1,15 @@
 package main
 
-import (
-	"encoding/json"
-	"os"
-	"time"
-)
+import "fmt"
+
 
 const json_path = "task_list.json"
 
 func main()  {
-
-	tasks := TaskList{
-		{
-			Task: TaskDetails{
-				Item: "cleaning",
-				CreatedAt: time.Now(),
-			},
-		},
-		{
-			Task: TaskDetails{
-				Item: "homework",
-				CreatedAt: time.Now().Add(time.Minute),
-			},
-		},
-		{
-			Task: TaskDetails{
-				Item: "eat",
-				CreatedAt: time.Now().Add(time.Minute*2),
-
-			},
-		},
-	}
-
-	f, err := os.OpenFile(json_path, os.O_RDWR | os.O_CREATE, 0)
-
+	err := DeleteTask(json_path, "1")
 	if err != nil {
 		panic(err)
 	}
-
-	defer f.Close()
-
-	tasks_json, err := json.MarshalIndent(tasks, "", "\t")
-
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = f.Write(tasks_json)
-	if err != nil {
-		panic(err)
-	}
-			
+	fmt.Println("task list updated")
 }
 
--- tasks.go
+++ tasks.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"os"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type TaskWrapper struct {
@@ -12,21 +16,86 @@ type TaskWrapper struct {
 type TaskDetails struct {
 	Item string `json:"item"`
 	CreatedAt time.Time `json:"created_at"`
+	Id string `json:"id"`
 }
 
 type TaskList []TaskWrapper
 
 
-func GetTasks() {
+func GetTasks(filePath string) (TaskList, error) {
+	
+	dat, err := os.ReadFile(filePath)
+	if err != nil {
+		return TaskList{}, err
+	}
 
+	currentTasks := TaskList{}
+	err = json.Unmarshal(dat, &currentTasks)
+	if err != nil {
+		return TaskList{}, err
+	}
+
+	return currentTasks, nil
 }
 
-func AppendTask() {
+func AddTask(filePath, title string) error { 
 
+	newTask := TaskWrapper{
+		Task: TaskDetails{
+			Item: title,
+			CreatedAt: time.Now(),
+			Id: uuid.NewString(),
+		},
+	}
+
+	taskList, err := GetTasks(filePath)
+	if err != nil {
+		return err
+	}
+
+	taskList = append(taskList, newTask)
+	dat, err := json.MarshalIndent(taskList, "", "\t")
+
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(json_path, dat, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func DeleteTask() {
+func DeleteTask(filePath, id string) error {
 
+	taskList, err := GetTasks(filePath)
+	if err != nil {
+		return err
+	}
+
+	updateTaskList := TaskList{}
+
+	for idx, task := range taskList {
+		if task.Task.Id == id {
+			continue
+		}
+		updateTaskList = append(updateTaskList, taskList[idx])
+	}
+	
+	dat, err := json.MarshalIndent(updateTaskList, "", "\t")
+
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(json_path, dat, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 
